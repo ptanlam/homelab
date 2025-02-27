5e6afc320660e9a77cd735ca24528df3635e761f
ptanlam <phanlam20033002@gmail.com>
2025-02-27 18:43:19 +0700

    add a lot of things

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { MeshCentral, Plex } from './app'
+import { LocalStack, MeshCentral, Plex, Yellowdig } from './app'
 import { Monitoring } from './monitoring'
 import { CertManager, Ingress, Pihole } from './networking'
 import { Storage } from './storage'
@@ -14,4 +14,8 @@ const storage = new Storage();
 const monitoring = new Monitoring({ rootCaCert: certManager.rootCaCert }, { dependsOn: [certManager, storage] });
 
 const plex = new Plex({ rootCaCert: certManager.rootCaCert }, { dependsOn: [certManager, storage] });
+
+const localstack = new LocalStack({ rootCaCert: certManager.rootCaCert }, { dependsOn: [certManager, storage] });
+const yellowdig = new Yellowdig({ rootCaCert: certManager.rootCaCert }, { dependsOn: [certManager, storage] });
+
 const meshcentral = new MeshCentral({ rootCaCert: certManager.rootCaCert }, { dependsOn: [certManager, storage] });
